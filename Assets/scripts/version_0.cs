ec1ca686f9f343c6a016cc3fc3ce8556432133bf
phyacy <1665116824@qq.com>
2025-02-18 20:01:34 +0800

    elden_ring

---
+++ Assets/scripts/version_0.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class version_0 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("×î³õµÄ°æ±¾");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
