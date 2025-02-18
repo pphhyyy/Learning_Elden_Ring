6c7c5615b9b2f7172482456a27a85a053da9ad64
phyacy <1665116824@qq.com>
2025-02-18 20:08:49 +0800

    utf-8

--- Assets/scripts/version_0.cs
+++ Assets/scripts/version_0.cs
@@ -7,7 +7,7 @@ public class version_0 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("×î³õµÄ°æ±¾");
+        Debug.Log("æœ€åˆçš„ç‰ˆæœ¬");
     }
 
     // Update is called once per frame
